86666438dfca88cf88eadc1d928fc43a988d125e
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 14:10:39 +0800

    [ParkingBoy][John Carlos]test: implement should return ticket when parking boy is given a car

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,11 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    final ParkingLot parkingLot;
+    public ParkingBoy(ParkingLot parkingLot){
+        this.parkingLot = parkingLot;
+    }
+    public ParkingTicket park(Car car){
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,17 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
-
+    void should_return_ticket_when_parking_boy_park_car_given_car() {
+        //given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        //when
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+        //then
+        assertNotNull(parkingTicket);
     }
 }
