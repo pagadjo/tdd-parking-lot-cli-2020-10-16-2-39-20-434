ef9c6a469b2964bb1e49fd7365e647230729b0c9
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 16:06:53 +0800

    [ParkingBoy][John Carlos]feature: return no car when fetch given ticket is used

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -13,6 +13,8 @@ public class ParkingLot {
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-        return mapTicketCar.get(parkingTicket);
+        Car car = mapTicketCar.get(parkingTicket);
+        mapTicketCar.remove(parkingTicket);
+        return car;
     }
 }
