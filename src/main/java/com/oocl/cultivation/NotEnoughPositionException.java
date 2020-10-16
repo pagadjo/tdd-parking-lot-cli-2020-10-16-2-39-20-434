0e6382cc5e2d019725e3c0b2ebf4b068f00b8e14
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 22:37:40 +0800

    [ParkingBoy][John Carlos]feature: created NotEnoughPositionException

---
+++ src/main/java/com/oocl/cultivation/NotEnoughPositionException.java
@@ -0,0 +1,9 @@
+package com.oocl.cultivation;
+
+public class NotEnoughPositionException extends RuntimeException {
+
+    public NotEnoughPositionException(String message) {
+        super(message);
+    }
+
+}
