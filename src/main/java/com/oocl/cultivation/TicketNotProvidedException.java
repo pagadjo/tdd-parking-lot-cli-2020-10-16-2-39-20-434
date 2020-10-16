0b48ad59f7af330af89b6275217109aeb30cad84
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 22:21:28 +0800

    [ParkingBoy][John Carlos]feature: created TicketNotProvidedException and UnrecognizedParkingTicketException

---
+++ src/main/java/com/oocl/cultivation/TicketNotProvidedException.java
@@ -0,0 +1,9 @@
+package com.oocl.cultivation;
+
+public class TicketNotProvidedException extends RuntimeException {
+
+    public TicketNotProvidedException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/oocl/cultivation/UnrecognizedParkingTicketException.java
@@ -0,0 +1,9 @@
+package com.oocl.cultivation;
+
+public class UnrecognizedParkingTicketException extends RuntimeException {
+
+    public UnrecognizedParkingTicketException(String message) {
+        super(message);
+    }
+
+}
