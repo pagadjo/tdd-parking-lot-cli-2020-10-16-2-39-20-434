7eee01be193d52cd13d40108e00c4d86e99e0a3f
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 17:52:43 +0800

    [ParkingBoy][John Carlos]feature: added Parking Lot constructor for capacity and boolean isParkingLotFull

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,17 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private final Map<ParkingTicket, Car> mapTicketCar = new HashMap<>();
+    private Map<ParkingTicket, Car> mapTicketCar = new HashMap<>();
+    private int capacity;
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
 
     public ParkingTicket park(Car car) {
-        ParkingTicket newTicket = new ParkingTicket();
-        mapTicketCar.put(newTicket, car);
-        return newTicket;
+        if (!isParkingLotFull()) {
+            ParkingTicket newTicket = new ParkingTicket();
+            mapTicketCar.put(newTicket, car);
+            return newTicket;
+        }
+        return null;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         Car car = mapTicketCar.get(parkingTicket);
         mapTicketCar.remove(parkingTicket);
         return car;
+    }
+
+    public Boolean isParkingLotFull() {
+        return mapTicketCar.size() == capacity;
     }
 }
