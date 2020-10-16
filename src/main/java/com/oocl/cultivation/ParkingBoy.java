a1d4eacd76896f8d415eaf4ef07ef67e0ec94781
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 21:52:05 +0800

    [ParkingBoy][John Carlos]refactor: added throw TicketNotProvidedException when parkingTicket isNull

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,15 +1,22 @@
 package com.oocl.cultivation;
 
+import static java.util.Objects.isNull;
+
 public class ParkingBoy {
     private final ParkingLot parkingLot;
-    public ParkingBoy(ParkingLot parkingLot){
+
+    public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
+
     public ParkingTicket park(Car car){
         return parkingLot.park(car);
     }
 
-    public Car fetch(ParkingTicket parkingTicket) {
+    public Car fetch(ParkingTicket parkingTicket){
+        if (isNull(parkingTicket)) {
+            throw new TicketNotProvidedException("Please provide your parking ticket.");
+        }
         return parkingLot.fetch(parkingTicket);
     }
 }
