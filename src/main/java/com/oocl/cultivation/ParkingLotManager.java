f3d71815954bffa6a0b4ffd795857d9242ec5ccf
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-18 20:11:56 +0800

    [ParkingBoy][John Carlos]refactor: add methods in ParkingLotManager

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -2,12 +2,25 @@ package com.oocl.cultivation;
 
 import java.util.List;
 
-public class ParkingLotManager extends ParkingBoy{
+public class ParkingLotManager extends ParkingBoy {
     private List<ParkingLot> parkingLotList;
+    private List<ParkingBoy> parkingLotBoysList;
+
     public ParkingLotManager(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
         this.parkingLotList = parkingLotList;
     }
+
+    public ParkingLotManager(List<ParkingBoy> parkingBoysList, List<ParkingLot> parkingLotList) {
+        super(parkingLotList);
+        this.parkingLotBoysList = parkingBoysList;
+        this.parkingLotList = parkingLotList;
+    }
+
+    public int parkingBoysListCount() {
+        return parkingLotBoysList.size();
+    }
+
     public ParkingTicket park(Car car) {
         if (parkingLotList.size() > 1) {
             for (ParkingLot parkingLot : parkingLotList) {
