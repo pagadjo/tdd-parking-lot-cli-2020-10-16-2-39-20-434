52cebf78d39db716eacedf8e580165b799d82513
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-19 23:10:35 +0800

    [ParkingBoy][John Carlos]refactor: separated most used objects

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -3,21 +3,23 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SmartParkingBoyTest {
+class SmartParkingBoyTest extends ParkingBoyTest{
+    private Car car1 = new Car();
+    private Car car2 = new Car();
+    private ParkingLot parkingLot1 = new ParkingLot();
+    private ParkingLot parkingLot2 = new ParkingLot();
+    private List<ParkingLot> parkingLotList = new ArrayList<>();
 
     @Test
     void should_park_in_second_parking_lot_when_parking_cars_given_second_parking_lot_has_more_spaces() {
         //given
-        Car car1 = new Car();
-        Car car2 = new Car();
-        Car car3 = new Car();
-        ParkingLot parkingLot1 = new ParkingLot(10);
-        ParkingLot parkingLot2 = new ParkingLot(15);
-        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLot1.setCapacity(10);
+        parkingLot2.setCapacity(15);
         parkingLotList.add(parkingLot1);
         parkingLotList.add(parkingLot2);
 
