6858b9e56b9386a9eeb02433e502e1a8a628c9ba
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-19 20:13:18 +0800

    [ParkingBoy][John Carlos]refactor: update UNRECOGNIZED_PARKING_TICKET

--- src/main/java/com/oocl/cultivation/Constants.java
+++ src/main/java/com/oocl/cultivation/Constants.java
@@ -2,6 +2,6 @@ package com.oocl.cultivation;
 
 public final class Constants {
     public static final String NOT_ENOUGH_POSITION = "Not Enough Position";
-    public static final String UNRECOGNIZED_PARKING_TICKET = "Unrecognized Parking Ticket";
+    public static final String UNRECOGNIZED_PARKING_TICKET = "Wrong Ticket! Please provide a correct one";
     public static final String PROVIDE_TICKET = "Please Provide your Parking Ticket";
 }
