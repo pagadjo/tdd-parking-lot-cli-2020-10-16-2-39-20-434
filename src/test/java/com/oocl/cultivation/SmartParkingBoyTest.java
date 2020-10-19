73fa0631bc5475a5d7a33bbabeba8650e001151e
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-19 19:29:19 +0800

    [ParkingBoy][John Carlos]refactor: change to if parkedCarsCount is equals to 2

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -9,21 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SmartParkingBoyTest {
 
-//    void parkManyCars(SmartParkingBoy smartParkingBoy, int carCount) {
-//        Car car = new Car();
-//        for (int x = 0; x <= carCount; x++) {
-//            smartParkingBoy.park(car);
-//        }
-//    }
-
     @Test
     void should_park_in_second_parking_lot_when_parking_cars_given_second_parking_lot_has_more_spaces() {
         //given
         Car car1 = new Car();
         Car car2 = new Car();
         Car car3 = new Car();
-        ParkingLot parkingLot1 = new ParkingLot(2);
-        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(15);
         List<ParkingLot> parkingLotList = new ArrayList<>();
         parkingLotList.add(parkingLot1);
         parkingLotList.add(parkingLot2);
@@ -32,10 +25,8 @@ class SmartParkingBoyTest {
         //when
         smartParkingBoy.park(car1);
         smartParkingBoy.park(car2);
-        ParkingTicket parkingTicketOfCar3 = smartParkingBoy.park(car3);
         //then
-        assertNotNull(parkingLot2.fetch(parkingTicketOfCar3));
+        assertEquals(2, parkingLot2.parkedCarsCount());
 
     }
-
 }
