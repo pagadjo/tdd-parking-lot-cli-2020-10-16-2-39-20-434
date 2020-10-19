c0b5dcaa630207d6bf4ea91a1c823a6e675464cc
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-19 23:34:42 +0800

    [ParkingBoy][John Carlos]refactor: modified test cases in ParkingLotManagerTest

--- src/test/java/com/oocl/cultivation/ParkingLotManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotManagerTest.java
@@ -9,15 +9,16 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotManagerTest {
-    ParkingLot parkingLot1 = new ParkingLot();
-    ParkingLot parkingLot2 = new ParkingLot();
-    ParkingLot parkingLot3 = new ParkingLot();
+    Car car = new Car();
+    private ParkingLot parkingLot1 = new ParkingLot();
+    private ParkingLot parkingLot2 = new ParkingLot();
+    private ParkingLot parkingLot3 = new ParkingLot();
+    private List<ParkingLot> parkingLotList = new ArrayList<>();
 
 
     @Test
     void should_return_ticket_when_parking_boy_park_car_given_car() {
         //given
-        Car car = new Car();
         parkingLot1.setCapacity(1);
         List<ParkingLot> parkingLotList = Collections.singletonList(parkingLot1);
 //
@@ -32,9 +33,8 @@ class ParkingLotManagerTest {
     @Test
     void should_return_correct_car_when_fetch_given_correct_ticket() {
         //given
-        Car car = new Car();
-        List<ParkingLot> parkingLotList = Collections.singletonList(new ParkingLot(10));
-        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLotList);
+        parkingLot1.setCapacity(10);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(Collections.singletonList(parkingLot1));
         ParkingTicket parkingTicket = parkingLotManager.park(car);
         //when
         Car expectedCar = parkingLotManager.fetch(parkingTicket);
@@ -45,20 +45,12 @@ class ParkingLotManagerTest {
     @Test
     void should_return_list_of_parking_boys_when_get_list_given_three_parking_boys() {
         //given
-        ParkingLot parkingLot1 = new ParkingLot(15);
-        ParkingLot parkingLot2 = new ParkingLot(20);
-        ParkingLot parkingLot3 = new ParkingLot(20);
-        List<ParkingLot> parkingLotList = new ArrayList<>();
-        List<ParkingBoy> parkingBoysList = new ArrayList<>();
-        parkingLotList.add(parkingLot1);
-        parkingLotList.add(parkingLot2);
-        parkingLotList.add(parkingLot3);
-//        when
         ParkingLotManager parkingLotManager;
         ParkingBoy parkingBoy = new ParkingBoy(Collections.singletonList(parkingLot1));
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Collections.singletonList(parkingLot2));
         SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(Collections.singletonList(parkingLot3));
         parkingLotManager = new ParkingLotManager(parkingLotList);
+        //when
         parkingLotManager.addParkingBoyToList(parkingBoy);
         parkingLotManager.addParkingBoyToList(smartParkingBoy);
         parkingLotManager.addParkingBoyToList(superSmartParkingBoy);
@@ -68,38 +60,27 @@ class ParkingLotManagerTest {
     }
 
     @Test
-    void should_return_error_message_when_manager_park_a_car_given_parking_boy_fetch_the_car() {
+    void should_return_error_message_when_failed_operation_given_parking_boy_fetch_the_car() {
         //given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
-        ParkingLot parkingLot3 = new ParkingLot(1);
-        List<ParkingLot> parkingLotList = new ArrayList<>();
-        List<ParkingBoy> parkingBoysList = new ArrayList<>();
-        parkingLotList.add(parkingLot1);
-        parkingLotList.add(parkingLot2);
-        ParkingLotManager parkingLotManager;
+        parkingLot1.setCapacity(1);
         ParkingBoy parkingBoy = new ParkingBoy(Collections.singletonList(parkingLot1));
-        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Collections.singletonList(parkingLot2));
-        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(Collections.singletonList(parkingLot3));
-        parkingBoysList.add(parkingBoy);
-        parkingBoysList.add(smartParkingBoy);
-        parkingBoysList.add(superSmartParkingBoy);
-        parkingLotManager = new ParkingLotManager(parkingLotList);
-        parkingLotManager.getParkingLotBoysList(parkingBoysList);
-//        when
-        Car car1 = new Car();
-        ParkingTicket parkingTicket = parkingLotManager.park(car1);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(Collections.singletonList(parkingLot1));
+        parkingLotManager.addParkingBoyToList(parkingBoy);
+        //when
+        ParkingTicket parkingTicket = parkingLotManager.park(car, parkingBoy);
+        ParkingTicket newParkingTicket = new ParkingTicket();
         //then
-
+        assertNotNull(parkingTicket);
         assertThrows(UnrecognizedParkingTicketException.class, () ->
-                smartParkingBoy.fetch(parkingTicket)
+                parkingLotManager.fetch(newParkingTicket, parkingBoy)
         );
     }
 
     @Test
     void should_parking_boy_fetch_car_when_manager_assign_parking_boy_and_car_given_parking_boy_and_a_car(){
         //given
-        ParkingLot parkingLot1 = new ParkingLot(1);
+
+        parkingLot1.setCapacity(1);
         ParkingBoy parkingBoy = new ParkingBoy(Collections.singletonList(parkingLot1));
         ParkingLotManager parkingLotManager = new ParkingLotManager(Collections.singletonList(parkingLot1));
         parkingLotManager.addParkingBoyToList(parkingBoy);
@@ -109,5 +90,4 @@ class ParkingLotManagerTest {
         //then
         assertNotNull(parkingLotManager.fetch(parkingTicket, parkingBoy));
     }
-
 }
